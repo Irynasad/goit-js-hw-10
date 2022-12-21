5dcb3a27d74502ddf837b96245a59912293a66de
Iryna Sadychenko <108194331+Irynasad@users.noreply.github.com>
2022-12-21 17:08:19 +0200

    update task

--- src/index.js
+++ src/index.js
@@ -1,50 +1,89 @@
 import './css/styles.css';
 import Notiflix from 'notiflix';
+import 'notiflix/dist/notiflix-3.2.5.min.css';
 import debounce from 'lodash.debounce';
 var debounce = require('lodash.debounce');
 const DEBOUNCE_DELAY = 300;
-const URL = 'https://restcountries.com/';
-const refs = {
-  input: document.querySelector('#search-box'),
-};
+// import { fetchCountries } from './fetchCountries';
+// const URL = 'https://restcountries.com/';
 
-const getItemTemplate = ({ country }) => `
-    <li class="country-item">
-      <img src="${country.flags.svg}" alt="Flag of ${country.name.official} height="30" width="40">
-      <h1 class="country-title">${country.name.official}}</h1>
+const getItemTemplate = ({ flags, name }) => `
+    <li class="country-item" data-id=${name}>
+      <img src="${flags.svg}" alt="Flag of ${name.official} width="30" height="20">
+      <h1 class="country-title">${name.official}</h1>
     </li>`;
 
 const getInfoOffItemTemplate = ({ capital, population, languages }) => `
   <p class="country-capital"><b>Capital</b>: ${capital}</p>
   <p class="country-population"><b>Population</b>: ${population}</p>
-  <p class="country-languages"><b>Languages</b>: ${languages} </p></li>`;
+  <p class="country-languages"><b>Languages</b>: ${languages} </p></li>
+  `;
 
-let countres = {};
+const clearName = () => {
+  refs.list.innerHTML = '';
+  refs.div.innerHTML = '';
+};
 
-fetch(`${URL}v3.1/name/sw`)
-  // fetch(
-  //   `${URL}v2/{service}?fields=name.official,capital,population,flags.svg,languages`
-  // )
-  .then(resp => resp.json())
-  .then(coutres => {
-    const markup = getItemTemplate(coutres);
-    if (countres.length === 1) {
-      const markupInfo = getInfoOffItemTemplate(countres[0]);
-    }
+const refs = {
+  input: document.querySelector('#search-box'),
+  list: document.querySelector('.country-list'),
+  div: document.querySelector('.country-info'),
+};
+let countries = [];
 
-    console.log(coutres);
-    console.log(markup);
-    console.log(markupInfo);
-  })
-  .catch(error => {
-    console.log(error);
-  });
+const renderList = () => {
+  // console.log(countries);
+  const listOfCountry = countries.map(getItemTemplate);
+  refs.list.innerHTML = '';
+  refs.list.insertAdjacentHTML('beforeend', listOfCountry.join(''));
+};
 
-// const onSearch = event => {
-//   event.preventDefault();
-//   //   refs.input.value = event.currentTarget.value;
-//   const value = event.target.value;
+const renderInfo = () => {
+  const infoOfCountry = countries.map(getInfoOffItemTemplate);
+  refs.div.innerHTML = '';
+  refs.div.insertAdjacentHTML('beforeend', infoOfCountry.join(''));
+};
 
-//   console.log('value:', value);
-// };
-// refs.input.addEventListener('input', debounce(onSearch, DEBOUNCE_DELAY));
+const onSearch = event => {
+  event.preventDefault();
+  let name = event.target.value.trim();
+
+  if (!name) {
+    clearName();
+    return;
+  }
+
+  fetch(`https://restcountries.com/v3.1/name/${name}`)
+    // fetch(
+    //   `https://restcountries.com/v2/all?fields=${name.official},${capital},${population},${flags.svg},${languages}`
+    // )
+    .then(resp => resp.json())
+    .then(data => {
+      console.log(data);
+      countries = data;
+      console.log(countries.length);
+      // renderList();
+      // renderInfo();
+      if (countries.length >= 10) {
+        return Notiflix.Notify.info(
+          `Too many matches found. Please enter a more specific name.`
+        );
+      }
+      if (countries.length > 2 && countries.length < 10) {
+        renderList();
+        return;
+      }
+      if (countries.length === 1) {
+        renderList();
+        renderInfo();
+        return;
+      }
+    })
+    .catch(() => {
+      clearName();
+      return Notiflix.Notify.failure(
+        `Oops, there is no country with that name`
+      );
+    });
+};
+refs.input.addEventListener('input', debounce(onSearch, DEBOUNCE_DELAY));
